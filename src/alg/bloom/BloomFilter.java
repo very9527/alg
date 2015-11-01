60f02d3c8761e9e0d5edd84e286a4348523d23a1
very9527 <very9527@flowerainss-MacBook-Pro.local>
2015-11-01 22:29:37 +0800

    update BloomFilter comment

--- src/alg/bloom/BloomFilter.java
+++ src/alg/bloom/BloomFilter.java
@@ -2,18 +2,20 @@ package alg.bloom;
 
 import java.util.BitSet;
 
-
+/**
+ * 
+ * @author very9527
+ *
+ */
 public class BloomFilter {
 	private static int[] seeds = {3,7,13,17,19,23};
 	private static String invalidStr = "tom@gmail.com";
 	private static int LENGHT = 2<<24;
 	private static BitSet sets = new BitSet(LENGHT);
 	
-	public static void main(String[] args) {
-		setValue();
-		check();
-	}
-	
+	/**
+	 * 将值hash后放入BitSet中
+	 */
 	public static void setValue() {
 		for (int i = 0; i < seeds.length; i++) {
 			int hashCode = hash(invalidStr, seeds[i]);
@@ -21,6 +23,9 @@ public class BloomFilter {
 		}
 	}
 	
+	/**
+	 * 检查某邮箱是否在过滤器中。
+	 */
 	public static void check() {
 		String checkValue = "tom1@gmail.com";
 		for (int i = 0; i < seeds.length; i++) {
@@ -33,11 +38,23 @@ public class BloomFilter {
 		}
 	}
 	
+	/**
+	 * 对字符串进行hash
+	 * @param hashStr
+	 * @param seed
+	 * @return
+	 */
 	public static int hash(String hashStr, int seed) {
 		int result = 0;
 		for(int i=0;i<hashStr.length(); i++) {
 			result = seed*result + hashStr.charAt(i);
 		}
 		return (LENGHT -1) & result;
+	}
+	
+	
+	public static void main(String[] args) {
+		setValue();
+		check();
 	}
 }	
